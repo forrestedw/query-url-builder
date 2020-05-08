b3f31bab1a73a561cfa4231956f5d157e0996d10
innoflash <innocentmazando@gmail.com>
2020-05-09 00:14:52 +0200

    added service provider

---
+++ src/QueryUrlBuilderServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Forrestedw\QueryUrlBuilder;
+
+use Illuminate\Support\ServiceProvider;
+
+class QueryUrlBuilderServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
