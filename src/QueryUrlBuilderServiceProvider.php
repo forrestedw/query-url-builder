d83b443376e1d7d071c9b3d0d83e0d1c228199ef
forrestedw <36917362+forrestedw@users.noreply.github.com>
2020-05-11 11:02:08 +0100

    Update QueryUrlBuilderServiceProvider.php

--- src/QueryUrlBuilderServiceProvider.php
+++ src/QueryUrlBuilderServiceProvider.php
@@ -13,7 +13,7 @@ class QueryUrlBuilderServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->app->bind('query-url-builder', function ($app) {
+        $this->app->bind(QueryUrlBuilder::class, function ($app) {
             return new QueryUrlBuilder();
         });
     }
